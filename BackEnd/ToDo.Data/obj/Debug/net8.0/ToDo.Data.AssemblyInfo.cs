dbfc82cd4c9bcf5149d01ce64ac05a778837b90f
borjamarser <zceeart@ucl.ac.uk>
2024-04-28 18:41:43 +0100

    Postgres setup

--- BackEnd/ToDo.Data/obj/Debug/net8.0/ToDo.Data.AssemblyInfo.cs
+++ BackEnd/ToDo.Data/obj/Debug/net8.0/ToDo.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+804b8be62429a609f615fdf689d900062822e00f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10b10ad3023e5922c03307d6acce24bca8c710a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
